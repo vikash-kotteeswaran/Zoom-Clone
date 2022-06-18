780969ddae1d5d404b6fd2be96f2f178815bc8f3
Newbie-master-416 <vikkykotty@gmail.com>
2022-06-18 16:18:10 +0530

    server.js has been edited to broadcast emit new user connection to the existing users

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const socket = require('socket.io')
+const io = socket(server);
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server, {debug: true})
 const {v4: uuidv4} = require('uuid');
 const PORT = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
     res.status(200);
@@ -16,5 +21,12 @@ app.get('/:room', (req, res) => {
     res.status(200);
     res.render('room', {roomId: req.params.room})
 })
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('new-user-connected', userId);
+    });
+});
 
 server.listen(PORT);
