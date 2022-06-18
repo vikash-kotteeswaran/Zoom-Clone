256815129e023e5262be18a3b6f5386c48cf185e
Newbie-master-416 <vikkykotty@gmail.com>
2022-06-18 16:24:01 +0530

    script.js has been edited to allow transfer of video and audio stream between different users

--- public/script.js
+++ public/script.js
@@ -1,8 +1,37 @@
-
+const socket = io('/');
 const Video = document.createElement("video");
+Video.id = 'User';
 const videoGrid = document.querySelector('#video-grid');
 
 Video.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true, 
+    audio: true
+}).then((stream) => {
+    
+    addVideoStream(Video, stream);
+
+    // WHY!!!!!! : https://stackoverflow.com/questions/66937384/peer-oncalll-is-never-being-called
+    
+    window.peer = new Peer(undefined, {
+        path: '/peerjs',
+        host: '/',
+        port: '3000'
+    })
+
+    peer.on('open', (userId) => {
+        socket.emit('join-room', roomId, userId);
+    })
+
+    connectMediaToNewUser(stream);
+
+    AnswerCallForFromUser(stream);
+
+    
+})
+
+
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
@@ -10,13 +39,35 @@ const addVideoStream = (video, stream) => {
         video.play();
     })
     videoGrid.append(video);
+
 }
 
-navigator.mediaDevices.getUserMedia({
-    video: true, 
-    audio: true
-}).then((stream) => {
-    addVideoStream(Video, stream);
+const connectMediaToNewUser = (stream) => {
+    socket.on('new-user-connected', (otherUserId) => {
+        const call = peer.call(otherUserId, stream);
+        const AnswererVideo = document.createElement("video");
+        AnswererVideo.id = 'otherUser:' + otherUserId;
+        AnswererVideo.muted = true;
+        call.on('stream', (AnswererStream) => {
+            addVideoStream(AnswererVideo, AnswererStream)
+        })
+        
+    })
+}
 
-})
+const AnswerCallForFromUser = (stream) => {
+    peer.on('call', call => {
+        call.answer(stream);
+        const CallerVideo = document.createElement("video");
+        CallerVideo.id = 'otherUser:' + call.peer;
+        CallerVideo.muted = true;
+        call.on('stream', (CallerStream) => {
+            addVideoStream(CallerVideo, CallerStream)
+        })
+
+    })
+}
+
+
+
 
